18bec1b4e86d427d5197aa97a6ce91d579b9c944
HJM <smjm0850@gmail.com>
2025-06-25 11:41:31 +0900

    localhost 변경

--- bonlunch/src/main/java/kr/smhrd/lunch/controller/GoodsController.java
+++ bonlunch/src/main/java/kr/smhrd/lunch/controller/GoodsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://52.79.46.16:5173")
 @RestController
 @RequestMapping("/api")
 public class GoodsController {
